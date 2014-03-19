1c1525c3f1393252b04412f0d05ef7593feb068b
Stephan Mestach <stephan.mestach@8thcolor.com>
2014-03-19 12:44:49 +0100

    require relative.

--- lib/pullreview/coverage.rb
+++ lib/pullreview/coverage.rb
@@ -1,4 +1,4 @@
-require 'pullreview/coverage/version'
-require 'pullreview/coverage/continuous_build'
-require 'pullreview/coverage/client_api'
-require 'pullreview/coverage/formatter'
+require_relative 'pullreview/coverage/version'
+require_relative 'pullreview/coverage/continuous_build'
+require_relative 'pullreview/coverage/client_api'
+require_relative 'pullreview/coverage/formatter'
