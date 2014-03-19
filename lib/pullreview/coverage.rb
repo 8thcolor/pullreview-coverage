d92263d8ec813c23b5adf7deba686d9f52f0816a
Stephan Mestach <stephan.mestach@8thcolor.com>
2014-03-19 22:00:29 +0100

    doc.

--- lib/pullreview/coverage.rb
+++ lib/pullreview/coverage.rb
@@ -1,5 +1,6 @@
-
+# https://www.pullreview.com
 module PullReview
+  # Collect coverage report to generate action points for the review.
   module Coverage
     # log to stdout message and optional exception at a given level
     def self.log(level, message, exception = nil)
